89070743be206a2d577df095088f5426fad76743
Take-san <take01.lapis@gmail.com>
2024-06-10 03:03:12 +0900

    Add ruby_activerecord_practice

---
+++ ruby_activerecord_practice/main.rb
@@ -0,0 +1,31 @@
+require 'yaml'
+require 'active_record'
+
+# DB 設定ファイル読込み
+config = YAML.load_file('./config.yml')
+ActiveRecord::Base.establish_connection(config['sqlite3']['development'])
+
+# プロジェクトテーブルのクラスを宣言
+class PROJECTS < ActiveRecord::Base
+    self.table_name = 'projects'
+    self.primary_key = :id
+end
+
+# タイプテーブルのクラスを宣言
+class TYPES < ActiveRecord::Base
+    self.table_name = 'types'
+    self.primary_key = :id
+end
+
+# プロジェクトテーブルの登録データを全件取得，反転，1件ずつ表示
+project_list = PROJECTS.all #project_list のクラスは ActiveRecord::Relation
+project_list_reverse = project_list.reverse #project_list_reverse のクラスは Array
+project_list_reverse.each do |project|
+    p project
+end
+
+# タイプテーブルの登録データを全件取得，反転，1件ずつ表示
+type_list = TYPES.all.to_a
+type_list.each do |type|
+    p type
+end
