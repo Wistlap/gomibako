2327549365e2de760d8ea86ab15d4a412f40fe4b
Take-san <take01.lapis@gmail.com>
2024-06-12 12:48:04 +0900

    Remove comment out part

--- character_code_analysis/moji_analyze.rb
+++ character_code_analysis/moji_analyze.rb
@@ -7,10 +7,3 @@ puts "1.txt: #{moji_code}"
 file_path = './docs/2.txt'
 moji_code = NKF.guess(File.read(file_path)).to_s
 puts "2.txt: #{moji_code}"
-
-# csv_file =
-#     if NKF.guess(File.read(file_path)).to_s == 'Shift_JIS'
-#         CSV.read(file_path, encoding: 'cp932')
-#     else
-#         CSV.read(file_path)
-#     end
